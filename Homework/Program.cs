22035d70ce2c055bd2c25f9a6d9bfb0da41800e7
Khalitov62 <tu7m5rk73code.edu.az>
2022-04-04 12:56:08 +0400

    Tasks added

---
+++ Homework/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = { 1, 5.3, 10, 99.99 };
+
+
+            Action<double[]> action = TotalPrice;
+            
+            
+
+            Action<double[]> action1 = Total;
+            action += action1;
+            action(array);
+            
+
+        }
+
+        public static void TotalPrice(double[] array)
+        {
+            
+            double total = default(double);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                total += array[i];
+
+                
+            }
+            Console.WriteLine(total);
+        }
+        
+        public static void Total(double[] array)
+        {
+            
+            int count = default(int);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                count++;
+               
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
